5279f6a65045f256e630f1872044793f0bc43cfe
Sushek <Sushek>
2019-02-13 09:48:10 +0545

    First Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>CollegeProject</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<Body>
+
+<form>
+	<input type="text" name="ID">
+	</form>
+
+
+
+	</Body>
+</html>
